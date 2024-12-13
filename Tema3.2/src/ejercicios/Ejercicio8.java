55221da1199d23f2da22de89ac7dbb96d2fa1a88
Samueljimenez444 <samuel.jimenez@iesnervion.es>
2024-12-13 14:39:03 +0100

    Ejercicios del 1 al 9

--- Tema3.2/src/ejercicios/Ejercicio8.java
+++ Tema3.2/src/ejercicios/Ejercicio8.java
@@ -1,21 +1,34 @@
 package ejercicios;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio8 {
 
 	public static void main(String[] args) {
-		String frase;
-		String palabra;
-		int comparacion;
-		Scanner lectura=new Scanner(System.in);
-		frase=lectura.nextLine();
-		for(int i=0;i<frase.length();i++){
-		if(frase.equals(" ")){
-			palabra+=frase.substring(0,i);
-		}
-		comparacion=palabra.compareTo(palabra);
-		}
+		 // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+        
+        // Pedir al usuario que ingrese una frase
+        System.out.println("Introduce una frase:");
+        String frase = scanner.nextLine();
+        
+        // Separar la frase en palabras usando el espacio como delimitador
+        String[] palabras = frase.split(" ");
+        
+        // Ordenar las palabras alfabéticamente
+        Arrays.sort(palabras);
+        
+        // Mostrar las palabras ordenadas
+        System.out.println("Palabras ordenadas alfabéticamente:");
+        for (String palabra : palabras) {
+            System.out.println(palabra);
+        }
+        
+        // Cerrar el scanner
+        scanner.close();
+    }
+
 	}
 
-}
+
---
+++ Tema3.2/src/ejercicios/Ejercicio9.java
@@ -0,0 +1,49 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio9 {
+
+	public static void main(String[] args) {
+		int valor;
+		String frase;
+		String traduccion;
+		int javalon=0;
+		int lenlen=0;
+		Scanner lectura=new Scanner(System.in);
+		System.out.println("Introduce la frase");
+		frase=lectura.nextLine();
+		valor=frase.indexOf("Javalin, javalon",0);
+		//Mientras el valor sea distinto a -1
+		while(valor>=0) {
+			//El numero de veces de la palabra aumenta en uno
+			//El valor pasa a valer el indice donde se encuentra la palabra
+			valor=frase.indexOf("Javalin, javalon",valor+1);
+			javalon++;
+		}
+		valor=frase.indexOf("Javalin, len, len",0);
+		//Mientras el valor sea distinto a -1
+		while(valor>0) {
+			//El numero de veces de la palabra aumenta en uno
+			//El valor pasa a valer el indice donde se encuentra la palabra
+			valor=frase.indexOf("Javalin, len, len",valor+1);
+			lenlen++;
+		}
+	if(javalon>0){
+		System.out.println("El dialecto es Javaliano");
+		traduccion=frase.substring(17,frase.length());
+		System.out.println(traduccion);
+	}
+	else if(lenlen>0){
+		System.out.println("El dialecto es Javalenlen");
+		traduccion=frase.substring(17,frase.length());
+		System.out.println(traduccion);
+		
+	}
+	else {
+		System.out.println("La frase no es de un dialecto Javaliano");
+		System.out.println(frase);
+	}
+	}
+
+}
