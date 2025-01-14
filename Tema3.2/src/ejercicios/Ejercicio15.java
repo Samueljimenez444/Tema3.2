c2d9c105d2735c8055ef75c6eeafc06b1fe9d538
Samueljimenez444 <samuel.jimenez@iesnervion.es>
2025-01-14 14:33:59 +0100

    Ejercicios

--- Tema3.2/src/ejercicios/Ejercicio15.java
+++ Tema3.2/src/ejercicios/Ejercicio15.java
@@ -1,12 +1,28 @@
 package ejercicios;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio15 {
 	public static String codificador(String frase) {
 		int auxiliar;
+		int random;
+		boolean igual=true;
+		Random rand=new Random();
+		random=rand.nextInt(1,frase.length());
+		char[]original=new char[frase.length()];
 		char[] codificar=new char[frase.length()];
 		codificar=frase.toCharArray();
+		original=frase.toCharArray();
+		
+		for(int i=0;i>frase.length();i++){
+		random=rand.nextInt(1,frase.length());
+		codificar[random]=codificar[i];
+		auxiliar=codificar[random];
+		codificar[i]=(char)auxiliar;
+				
+		}
 		
 		return frase;
 	}
@@ -18,6 +34,7 @@ public class Ejercicio15 {
 	Scanner lectura=new Scanner(System.in);
 	System.out.println("Introduce una frase");
 	frase1=lectura.nextLine();
+	System.out.println(codificador(frase1));
 	}
 
 }
